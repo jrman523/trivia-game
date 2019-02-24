402e2e38052e9e54d0f4770c496f011f2f80e231
Jad El Ramadi <jadelramadi@gmail.com>
2019-02-23 23:24:29 -0500

    fixed the issue with validation and added score

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,59 +1,72 @@
 var rsp = window.array;
 var rand;
-var intervalId = false;
+var timer;
+var questionIndex = 0;
+var maxQuestions = window.array.length;
+var correctAns = 0;
+var incorrectAns = 0;
+var unanswered = 0;
+var startOver = false;
 
-function load() {
-  rand = Math.floor(Math.random() * rsp.length);
-  $('#question').text(rsp[rand].question);
-  for (let i = 0; i < rsp[rand].answer.length; i++) {
-    var btn = $('<button class="btn">');
-    btn.text(rsp[rand].answer[i].value);
-    btn.attr('value', rsp[rand].answer[i].correct);
-    $('#answer').append(btn);
-  }
-  intervalId = false;
-}
-load();
-var time = 60;
-var timer = setInterval(function () {
-  $("#time").text('Time Left: ' + time-- + ' Seconds');
-  if (time === 0) {
-    clearInterval(timer);
-    intervalId = true;
-    $("#time").text('Time Out');
-  }
-}, 1000);
-
-function reset() {
-  $('#answer').html('');
-  $('#question').html('');
-  load();
-  time = 60;
+function gameTimer (){
+  var time = 5;
   timer = setInterval(function () {
     $("#time").text('Time Left: ' + time-- + ' Seconds');
-    if (time === 0) {
-      clearInterval(timer);
-      intervalId = true;
+    console.log("Time: " + time);
+    if (time == 0) {
+      var correctIndex = rsp[questionIndex].index;
+      console.log("Time is zero: " + time);
       $("#time").text('Time Out');
+      unanswered++;
+      $('#answer').html('');
+      $('#question').text('Sorry The Right Answer is ' + rsp[questionIndex].answer[correctIndex].value);
+      clearInterval(timer);
+      setTimeout(function () { reset(); }, 2000);
     }
   }, 1000);
 }
 
-$(".btn").click(function () {
-  var correctAns = rsp[rand].index;
+function load() {
+  gameTimer();
+  $('#question').text(rsp[questionIndex].question);
+  for (let i = 0; i < rsp[questionIndex].answer.length; i++) {
+    var btn = $('<button class="btn">');
+    btn.text(rsp[questionIndex].answer[i].value);
+    btn.attr('value', rsp[questionIndex].answer[i].correct);
+    $('#answer').append(btn);
+  }
+}
+load();
+
+function reset() {
+  $('#answer').html('');
+  $('#question').html('');
+  if(questionIndex >= (maxQuestions-1)){
+    $('#question').text('All done, here is how you did!');
+    $('#correctAns').text('Correct Answers : ' + correctAns);
+    $('#incorrectAns').text('Incorrect Answers : ' + incorrectAns);
+    $('#Unanswered').text('Unanswered : ' + unanswered);
+  } else {
+    questionIndex++;
+    load();
+  }
+}
+
+$(document).on('click', 'button', function (e){
+  clearInterval(timer);
+  var correctIndex = rsp[questionIndex].index;
+  console.log("This is the value: " + this.value);
   var val = this.value;
-  if (val == true) {
+  if (val == "true") {
     $('#answer').html('');
     $('#question').text('You Got It');
+    correctAns++;
   } else {
     $('#answer').html('');
-    $('#question').text('Sorry The Right Answer is ' + rsp[rand].answer[correctAns].value);
+    $('#question').text('Sorry The Right Answer is ' + rsp[questionIndex].answer[correctIndex].value);
+    incorrectAns++;
   }
-  if (intervalId == true) {
-    $('#answer').html('');
-    $('#question').text('Sorry The Right Answer is ' + rsp[rand].answer[correctAns].value);
-  }
+  e.preventDefault();
   setTimeout(function () { reset(); }, 2000);
 });
-
 
